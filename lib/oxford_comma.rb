41d0413111b46ac786e90fe5bd6a0e5967c2b08d
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-05 02:42:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.length == 3
    array[0..1].join(", ")
-   array[-1].join(" and ")
+   array[-1]  << " and "
   else
     array.last.join (" and ")
   end
