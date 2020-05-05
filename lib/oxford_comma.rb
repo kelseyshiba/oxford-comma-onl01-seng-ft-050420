96143f49f2571edb642a86c267fa51c7de4612d2
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-05 02:52:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,10 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2 
     array.join(" and ")
-  elsif array.length == 3
+  else 
+    
+    
+    array.length == 3
    if array != array.last
      array.join(", ")
    else 
