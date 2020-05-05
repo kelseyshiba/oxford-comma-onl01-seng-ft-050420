c473d0534448b73c3250fbc2c787e7144fbe1246
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-05 02:25:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,8 @@ def oxford_comma(array)
   elsif array.length == 2 
     array.join(" and ")
   elsif array.length == 3
+    if array.last
+      
    array[0, 1].join(", ")
    array[2].join(" and ")
   else
